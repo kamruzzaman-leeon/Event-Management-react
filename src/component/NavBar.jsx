c185ea50343e7944f0f2c2a9c3d4bf314c60116f
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-10-20 20:49:32 +0600

    image of avater & logiout button added inside navbar end

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -28,9 +28,19 @@ const NavBar = () => {
   </div>
   <div className="navbar-end">
     <ul className="menu menu-horizontal px-1">
-        <li className="md:mx-5"><NavLink to="/login">Login</NavLink></li>
-        <li className="md:mx-5"><NavLink to="/Register">Register</NavLink></li>
+        <li className="md:mx-2"><NavLink to="/login">Login</NavLink></li>
+        <li className="md:mx-2"><NavLink to="/Register">Register</NavLink></li>
     </ul>
+    <div className="dropdown dropdown-end">
+    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://images.app.goo.gl/Ub7B2r2xf4Z7ojvT9" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
     
   </div>
 </div>
