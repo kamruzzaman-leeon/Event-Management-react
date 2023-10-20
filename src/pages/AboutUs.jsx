c08fee6f703f946ed39c492b23e1d326b27af442
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-10-21 04:39:40 +0600

    login page added

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,10 +3,12 @@ import { Helmet } from "react-helmet";
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className="container mx-auto">
             <Helmet>
                 <title>About Us</title>
             </Helmet>
+            <h1 className="text-5xl font-medium my-5">Event Master</h1>
+            <p className="text-justify">At Event Master, we are dedicated to delivering unparalleled corporate event management services. We specialize in a wide range of corporate events, from conferences and seminars to award ceremonies, providing our clients with tailored solutions to meet their unique needs. Our commitment to excellence, attention to detail, and passion for creating memorable experiences set us apart as the leading event management company in the industry.</p>
         </div>
     );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,34 @@
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto">
+           <div className="hero min-h-screen bg-base-100">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+   
+    <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          {/* <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label> */}
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
